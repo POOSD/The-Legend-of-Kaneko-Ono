b62019a14847dd03e180001bdbf16481ef59ee12
Daniel-Trimble <trimbled@knights.ucf.edu>
2021-02-13 23:21:50 -0500

    Updated demo level, should work now.

---
+++ Project/Assets/Code/Environmental_Variables_Container.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Environmental_Variables_Container : MonoBehaviour
+{
+    public int numberPlayerCharacters;
+    public int numberEnemyCharacters;
+    public int score;
+    public int difficulty;
+    DateTime startTime;
+    DateTime currentTime;
+    public float velocityModifier;
+    public float driftModifier;
+    public float scrollSpeed;
+    public bool playerVulnerability;
+    public bool bossVulnerability;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        numberPlayerCharacters = 0;
+        numberEnemyCharacters = 0;
+        score = 0;
+        // difficulty = getDifficulty();
+        startTime = System.DateTime.Now;
+        velocityModifier = 0;
+        driftModifier = 0;
+        // scrollSpeed = getScrollSpeed();
+        playerVulnerability = false;
+        bossVulnerability = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
