743314f566c1a61225fc71324dcd13c354366b1b
KelvinCrank22 <49009119+KelvinCrank22@users.noreply.github.com>
2021-03-21 10:57:14 -0400

    Update healthCheckTest.cs

--- Project/Assets/Tests/healthCheckTest.cs
+++ Project/Assets/Tests/healthCheckTest.cs
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -22,3 +23,29 @@ namespace Tests
         }
     }
 }
+=======
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NUnit.Framework;
+namespace Tests
+{
+    public class healthCheckTest : MonoBehaviour
+    {
+        [Test]
+        public void healthNegativeValueException()
+        {
+            var healthy = new GameObject().AddComponent<health>();
+            Assert.Throws<ArgumentOutOfRangeException>(() => healthy.TakeDamage(10));
+        }
+
+        [Test]
+        public void healthNegativeValueExceptionTwo()
+        {
+            var healthy = new GameObject().AddComponent<health>();
+            Assert.Throws<ArgumentOutOfRangeException>(() => healthy.TakeDamage(-10));
+        }
+    }
+}
+>>>>>>> Stashed changes
