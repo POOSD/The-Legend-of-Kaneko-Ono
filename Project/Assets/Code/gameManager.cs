23ffc4ec2cbc2ae4aa8a874b7de852eaccd30c04
shirnava <shirnava@gmail.com>
2021-03-25 13:35:28 -0400

    Reset Number of Enemies Killed When Game Ends

--- Project/Assets/Code/gameManager.cs
+++ Project/Assets/Code/gameManager.cs
@@ -8,6 +8,8 @@ public class gameManager : MonoBehaviour
 {
     public void gameOver(string s){
         Debug.Log("GAME OVER");
+        //reset number of enemies killed to 0
+        Environmental_Variables_Container.numberEnemiesKilled=0;
 
         //if player health is 0, then this function is called with string "lose"
         if(s=="lose")
