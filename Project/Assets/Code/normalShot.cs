4251b326ac982af8809c670287cd54839adf7b99
KelvinCrank22 <49009119+KelvinCrank22@users.noreply.github.com>
2021-03-26 22:26:54 -0400

    fixed some git errors in normalShot

--- Project/Assets/Code/normalShot.cs
+++ Project/Assets/Code/normalShot.cs
@@ -21,19 +21,15 @@ public class normalShot : MonoBehaviour
    void OnTriggerEnter2D(Collider2D hitInfo)
    {
       Enemy enemy = hitInfo.GetComponent<Enemy>();
-<<<<<<< HEAD
-
-=======
       if(enemy != null){
->>>>>>> e3f6096cef3fb34feb7974a712f38fc82103bad2
-      if (enemy.health > 0)
-      {
-         enemy.TakeDamage(damage);
-         curHealth -= damage;
-      }
+        if (enemy.health > 0)
+        {
+           enemy.TakeDamage(damage);
+           curHealth -= damage;
+        }
 
       Debug.Log(enemy.name + " " + damage + " " + enemy.health);
-        
+
 
       Instantiate(impactEffect, transform.position, transform.rotation);
 
