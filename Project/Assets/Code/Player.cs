a000e253031773525f2904b783b4530fc51b7fca
KelvinCrank22 <49009119+KelvinCrank22@users.noreply.github.com>
2021-03-27 20:18:21 -0400

    undoing a dumb revert

---
+++ Project/Assets/Code/Player.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+   public int health = 100;
+   public GameObject deathEffect;
+
+
+
+//healthBarRect.sizeDelta = new Vector2(0.96f, 0.19f);
+
+//healthBarRect.sizeDelta = new Vector2(0.96f - (curHealth / 100), healthBarRect.sizeDelta.y);
+
+   public void TakeDamage(int damage)
+   {
+      health -= damage;
+
+      if (health <= 0)
+      {
+         Die();
+      }
+   }
+
+   void Die()
+   {
+      Instantiate(deathEffect, transform.position, Quaternion.identity);
+      
+      //update environment variables accordingly
+   }
+
+//    internal void TakeDamage(float damage)
+//    {
+//        throw new NotImplementedException();
+//    }
+}
