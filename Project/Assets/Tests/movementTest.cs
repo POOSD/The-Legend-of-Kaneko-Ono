df11e7fb59c6879c30f2205f00dbcb2e15056e8e
md7803 <megan@eci-web.com>
2021-03-28 12:45:26 -0400

    Created movement test

--- Project/Assets/Tests/movementTest.cs
+++ Project/Assets/Tests/movementTest.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
+using NUnit.Framework;
 using UnityEngine;
+using UnityEngine.TestTools;
 
 public class movementTest : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+   [UnityTest]
+   public IEnumerator movementTest()
+   {
+       // Generate a ship object
+        GameObject shipObject1 = new GameObject();
+        GameObject shipObject2 = new GameObject();
+        shipObject1.AddComponent<enemyMovement>();
+        shipObject2.AddComponent<enemyMovement>();
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+        shipObject1.position.transform.x = -1;
+        shipObject1.position.transform.y = 1;
+        shipObject1.position.transform.z = 0;
+
+        shipObject2.position.transform.x = 0;
+        shipObject2.position.transform.y = 0;
+        shipObject2.position.transform.z = 0;
+        shipObject2.valueX = -1;
+        shipObject2.valueY = 1;
+
+        shipObject2.Move();
+
+        Assert.AreEqual(shipObject1.position.transform, shipObject2.position.transform);
+
 }
