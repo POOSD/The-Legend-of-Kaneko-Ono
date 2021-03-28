41d3e02cf7bfa3e9547d0be8d478aa17f4e47eaa
KelvinCrank22 <49009119+KelvinCrank22@users.noreply.github.com>
2021-03-28 13:54:57 -0400

    added a couple comments to normalShot

--- Project/Assets/Code/normalShot.cs
+++ Project/Assets/Code/normalShot.cs
@@ -16,7 +16,7 @@ public class normalShot : MonoBehaviour
    {
       curHealth = 100;
       rigidBody.velocity = transform.right * speed;
-      Destroy(gameObject, 2);
+      Destroy(gameObject, 2); // despawns after 2 secs in case it takes too long
    }
 
    void OnTriggerEnter2D(Collider2D hitInfo)
@@ -35,7 +35,7 @@ public class normalShot : MonoBehaviour
       var impact = Instantiate(impactEffect, transform.position, transform.rotation);
 
       Destroy(gameObject);
-      Destroy(impact, 1);
+      Destroy(impact, 1); // the impact is still there even after ending
       }
    }
 }
